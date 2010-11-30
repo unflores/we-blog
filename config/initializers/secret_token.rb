f4e78da5701a335216de2274e04006cc47913ad7
A Flores <ctrl4ltdeleteme@gmail.com>
2010-11-30 02:06:18 -0500

    first!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = 'ce04ad6d3eeee39a40abc58e3e6a7d1f8fb7e04aff56fc647a70c46770f8bba581900f614a5566e7430975d94ec3a650a8b382db32ce7f03d04dc1f053d63f52'
