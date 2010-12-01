06334c1c6ac797ddcf8c60feb2d23ee6ee77268d
A Flores <ctrl4ltdeleteme@gmail.com>
2010-12-01 15:26:44 -0500

    addin some helpers n stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def navigation_items
+    @navigation_items ||= Post.for_nav
+  end
+
+  # def tweets
+  #   @tweets ||= Tweet.front_page
+  # end
+
+  def admin_required
+    unless admin?(session)
+      flash[:error] = "You must be logged in to access this page."
+      redirect_to '/sessions/new'
+    end
+  end
+
+  def admin?(session)
+    session[:password] == "hai!!!1111lo"  
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def linkify(text)
+    text.gsub(/(http:\/\/[^\s]*)/,'<a href="\1" target="_blank">\1</a>') unless text[/<a[^>]*>/]
+  end
 end
