6f62c8ccf5b1d3588befed84ea4d8afeb29f136f
A Flores <ctrl4ltdeleteme@gmail.com>
2010-11-30 22:44:55 -0500

    add tweets schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101201031104) do
+ActiveRecord::Schema.define(:version => 20101201033628) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body"
     t.boolean  "published"
     t.boolean  "in_navigation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tweets", :force => true do |t|
+    t.string   "status"
+    t.datetime "twitter_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
