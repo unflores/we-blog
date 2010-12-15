18a274df1d8ccf7947ebd9dbd9407bab19e33543
A Flores <ctrl4ltdeleteme@gmail.com>
2010-12-15 00:29:39 -0500

    Added tagging to posts, added tagging display, updated styles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  acts_as_taggable
+
   validates_presence_of :body, :title
   
   scope :on_frontpage, where(:published => true).limit(5).order('posts.created_at DESC')
