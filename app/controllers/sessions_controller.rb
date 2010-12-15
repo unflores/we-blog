735228f01e99e98e5b42b9e45bba912140a2053d
A Flores <ctrl4ltdeleteme@gmail.com>
2010-12-14 23:41:16 -0500

    updated redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
   def create  
     session[:password] = params[:password]  
     flash[:notice] = 'Successfully logged in'  
-    redirect_to :controller => 'posts', :action => 'display'
+    redirect_to posts_path
   end  
     
   def destroy  
     reset_session
     flash[:notice] = 'Successfully logged out'  
-    redirect_to :controller => 'posts', :action => 'display'
+    redirect_to root_path
   end  
 end
