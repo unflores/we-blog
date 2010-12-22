f2daf00852c4439f0f3609a3924e250b22c2bc5c
A Flores <ctrl4ltdeleteme@gmail.com>
2010-12-22 01:12:51 -0500

    refactored some controller code added an rss feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
-  before_filter :admin_required, :except => [ :display, :show, :tags ]
+  before_filter :admin_required, :only => [ :index, :new, :edit,:create, :destroy, :update ]
+  before_filter :get_resource, :only => [:show, :edit, :destroy, :update]
   def display
     @posts = Post.on_frontpage
-  end
-
-  def show
-    @post = Post.find(params[:id])
+    respond_to do |format| 
+      format.html
+      format.rss{ redirect_if_direct_request }
+    end 
   end
   
   def index
@@ -16,17 +17,11 @@ class PostsController < ApplicationController
     @post = Post.new()
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
-
   def create
     @post = Post.new(params['post'])
-    if @post.save()
-      redirect_to posts_path
-    else
-      redirect_to new_post_path
-    end
+    if @post.save() 
+    then redirect_to posts_path
+    else redirect_to new_post_path end
   end
 
   def tags
@@ -35,7 +30,6 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
     respond_to do |format|
       if @post.update_attributes(params[:post])
         format.html { redirect_to(@post, :notice => 'Post was successfully updated.') }
@@ -48,9 +42,16 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params['id'])
     @post.delete()
     redirect_to posts_path
   end
 
+  private 
+    def redirect_if_direct_request
+      unless request.env['HTTP_USER_AGENT'] =~ /feedburner/i 
+        # redirect_to 'http://feeds.feedburner.com/' and return
+      end 
+    end
+
+    def get_resource; @post = Post.find(params[:id]) end
 end
