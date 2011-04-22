9507e433304698f4b3b4ca1dd0ef74bd19bf98d4
A Flores <ctrl4ltdeleteme@gmail.com>
2011-04-22 22:33:59 +0200

    updated some styles added links to twitter users for the tweet stack

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
   def linkify(text)
-    text.gsub(/(http:\/\/[^\s]*)/,'<a href="\1" target="_blank">\1</a>') unless text[/<a[^>]*>/]
+    text.gsub(/(http:\/\/[^\s]*)/,'<a href="\1" target="_blank">\1</a>') unless anchor_exists text
   end
+
+  def linkify_tweet text
+    text.gsub(/@([a-zA-Z\_]+)/, '<a href="http://twitter.com/#!/\1">@\1</a>') unless anchor_exists text
+  end
+
+  def anchor_exists text
+    not text[/<a[^>]*>/].nil?
+  end
+
 end
