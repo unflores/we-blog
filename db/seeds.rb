d1a067c97903da53cb59f8fe425e6d4d13c96d63
Austin Flores <ctrl4ltdeleteme@gmail.com>
2015-10-29 17:16:25 +0100

    Added a few posts to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Post.create(
+  title: "Some Name",
+  body: "Finding out if data has changed Rails uses changed? Unfortunately, tainted? doesn't seem to work for activerecord objects.",
+  in_navigation: false,
+  published: true
+)
+
+Post.create(
+  title: "How to Add Lists to the Descriptions in Pivotal Tracker <a href='http://community.pivotaltracker.com/pivotal/topics/is_there_a_way_to_use_bullets_in_the_description_field_of_a_story' title=\"you're killing me buster!\">IMPOSSIBLE.</a>",
+  body: "Kind of a bummer. Programmers work with lists, it's a well known fact.  On the plus side, they do offer a tasks functionality, but sometimes I just don't feel like checking things off.",
+  in_navigation: false,
+  published: true
+)
+
